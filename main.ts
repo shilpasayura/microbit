d3f18653012e39b807e520028c673a65a7e24ff3
shilpasayura <shilpasayura@gmail.com>
2020-07-03 18:26:12 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showNumber(1)
+basic.pause(300)
+basic.showString("Nira")
+basic.pause(300)
+
+basic.forever(function () {
+    basic.showNumber(2)
+    basic.showIcon(IconNames.Angry)
+    basic.showArrow(ArrowNames.NorthWest)
+})
